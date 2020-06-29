28413bede731e0af3f4e54339e38899e8fed8e3d
btowns <bill.townsley@nike.com>
2020-06-29 15:24:01 -0700

    adding today's updates

--- class_project/my_class_file.py
+++ class_project/my_class_file.py
@@ -1,21 +1,38 @@
 class Test:
-    a = "class_variable_a"
-    b = "class_variable_b"
 
     def __init__(self, a):
-        print("Default class variable {}".format(self.a))
-        self.a = a
-        print("Override default class variable {}".format(self.a))
-        self._x = "instance_variable_x"
-        print("Created an instance variable {}".format(self._x))
-        self.__y = "private_instance_variable__y"
-        print("Created a private variable {}".format(self.__y))
-        print("Created a private variable {}".format(self._Test__y))
-        b = "local_variable_b"
-        print("Created a local variable {}".format(self.b))
-        print("Created a local variable {}".format(Test.b))
-        print("Created a local variable {}".format(b))
+        self._a = a
+        self._b = "instance_variable_b"
 
+    @property
+    def a(self):
+        print("getter of a")
+        return self._a
+
+    @a.setter
+    def a(self, value):
+        print("setter of a")
+        self._a = value
+
+    @a.deleter
+    def a(self):
+        print("deleter of a")
+        del self._a
+
+    @property
+    def b(self):
+        print("getter of b")
+        return self._b
+
+    @a.setter
+    def b(self, value):
+        print("setter of b")
+        self._b = value
+
+    @a.deleter
+    def b(self):
+        print("deleter of b")
+        del self._b
 
 def main():
     return
@@ -25,8 +42,7 @@ if __name__ == "__main__":
     main()
 
 my_test = Test("main_variable")
-print(my_test.a)
-print(my_test._x)
-print(my_test._Test__y)
-print(Test.b)
-print(my_test.b)
+print(my_test._a)
+my_test._a = "foo"
+print(my_test._a)
+print(my_test._b)
